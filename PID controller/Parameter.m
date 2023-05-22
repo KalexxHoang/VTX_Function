5c54ddf1d85ab3be0f660f9848f007f315aca899
KalexxHoang <hoangvanlam.iq2000@gmail.com>
2023-05-22 17:31:38 +0700

    22/5/2023

---
+++ PID controller/Parameter.m
@@ -0,0 +1,13 @@
+clear;clc;close all
+
+%% Inertial Matrix
+    I = diag([5 8 7]);
+    
+%% PD Parameter   
+    A_PD = [zeros(3) 0.5*eye(3);zeros(3,6)];
+    B_PD = [zeros(3);I];
+    
+    Q_PD = eye(6);
+    R_PD = eye(3);
+    
+    [K_PD,S,E] = lqrd(A_PD,B_PD,Q_PD,R_PD,0.7);
